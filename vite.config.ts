f87080517773f946177afca3c6e111031f0303fa
Juan Chuc <dev.juan.eloper@gmail.com>
2024-11-19 21:53:45 -0600

    feature: base project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~src": resolve(__dirname, "src/"),
+    },
+  },
   plugins: [react()],
-})
+});
